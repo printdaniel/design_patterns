4a0aec10849b0b8747289cf6ce35b254b67a495b
Daniel <dwsuarez98@gmail.com>
2024-04-29 14:42:27 -0300

    update: singleton

--- Singleton/example_singleton.py
+++ Singleton/example_singleton.py
@@ -1,20 +1,17 @@
-""" 
+"""
+El patrón de diseño Singleton es un patrón de software que garantiza que una
+clase sólo tenga una única instancia y proporciona un punto de acceso global a
+ella. En Python, esto se puede lograr mediante la creación de una clase con un
+método new que devuelva la misma instancia cada vez que se llama.
 
-El patrón de diseño Singleton es un patrón de software que garantiza que una 
-clase sólo tenga una única instancia y proporciona un punto de acceso global a 
-ella. En Python, esto se puede lograr mediante la creación de una clase con un 
-método new que devuelva la misma instancia cada vez que se llama. 
-
-En este ejemplo, se utiliza la variable de clase __instance para almacenar la 
-única instancia de la clase Singleton. Cada vez que se llama a Singleton(), 
-el método __new__ se ejecuta y devuelve Singleton.__instance si ya existe, 
-o crea una nueva instancia y la devuelve si no existe. De esta manera, 
+En este ejemplo, se utiliza la variable de clase __instance para almacenar la
+única instancia de la clase Singleton. Cada vez que se llama a Singleton(),
+el método __new__ se ejecuta y devuelve Singleton.__instance si ya existe,
+o crea una nueva instancia y la devuelve si no existe. De esta manera,
 se garantiza que sólo exista una única instancia de la clase en todo momento.
 
 """
-
 import sqlite3
-
 
 class DatabaseConnection:
     __instance = None
@@ -47,8 +44,9 @@ conn2 = DatabaseConnection()
 cursor2 = conn2.connect("database.db")
 print(cursor2)  # Already connected to the database
 # <sqlite3.Cursor object at 0x...>
-""" Mediante uso de un decorador, para que sea más 'pythonico'"""
 
+# ----------------------------------------------------------------------------
+""" Mediante uso de un decorador, para que sea más 'pythonico'"""
 
 def singleton(cls):
     instances = {}
@@ -87,3 +85,18 @@ conn2 = DatabaseConnection()
 cursor2 = conn2.connect("database.db")
 print(cursor2)  # Already connected to the database
 # <sqlite3.Cursor object at 0x...>
+
+# ----------------------------------------------------------------------------
+"""Impementación clasica de Singelton"""
+class Singleton ():
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(Singleton, cls).__new__(cls)
+        return cls.instance
+
+
+s = Singleton()
+print("Objeto Creado", s)
+
+s1 = Singleton()
+print("Objeto Creado", s1)
